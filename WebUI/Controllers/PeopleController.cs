124f904cab72157298f3b497b3f71e581901f6f3
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 22:58:43 +0300

    PeopleController was completed

--- WebUI/Controllers/PeopleController.cs
+++ WebUI/Controllers/PeopleController.cs
@@ -1,7 +1,9 @@
-﻿using Application.People.Queries.GetAllPeople;
-using Domain.Entities;
+﻿using Application.Common.Exceptions;
+using Application.People.Commands.CreatePerson;
+using Application.People.Commands.DeletePerson;
+using Application.People.Commands.UpdatePerson;
+using Application.People.Queries.GetAllPeople;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
@@ -9,10 +11,47 @@ namespace WebUI.Controllers
     public class PeopleController : ApiControllerBase
     {
         [HttpGet]
-        public async Task<IEnumerable<Person>> GetAllPeople()
+        public async Task<IActionResult> GetAllPeople()
         {
             var query = new GetAllPeopleQuery();
-            return await Mediator.Send(query);
+            return Ok(await Mediator.Send(query));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePerson(CreatePersonCommand command)
+        {
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePerson(UpdatePersonCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+            }
+            catch (EntityNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePerson(DeletePersonCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+            }
+            catch (EntityNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
