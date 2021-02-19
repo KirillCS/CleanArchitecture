25c6a654e4ff165baf1629839b4b37393bee50ec
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 17:08:05 +0300

    IApplicationDbContext was added

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    interface IApplicationDbContext
+    {
+        DbSet<Person> People { get; set; }
+
+        Task<int> SaveChanges(CancellationToken cancellationToken);
+    }
+}
