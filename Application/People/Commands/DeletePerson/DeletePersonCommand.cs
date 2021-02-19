b09dd4b6c307dcd6cf45e2af33626d74cd041ea7
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 17:46:20 +0300

    DeletePersonCommand was added

---
+++ Application/People/Commands/DeletePerson/DeletePersonCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.People.Commands.DeletePerson
+{
+    public class DeletePersonCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeletePersonCommandHandler : IRequestHandler<DeletePersonCommand>
+    {
+        private readonly IApplicationDbContext context;
+
+        public DeletePersonCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(DeletePersonCommand request, CancellationToken cancellationToken)
+        {
+            var person = await context.People.FindAsync(request.Id);
+            if (person == null)
+            {
+                throw new EntityNotFoundException(nameof(Person), request.Id.ToString());
+            }
+
+            context.People.Remove(person);
+            await context.SaveChanges(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
