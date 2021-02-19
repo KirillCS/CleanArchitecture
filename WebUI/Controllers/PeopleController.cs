b900ea255d34f5c4e30803d9e73f4962be55c59b
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 22:40:17 +0300

    Base class with isender property for api controller was added

--- WebUI/Controllers/PeopleController.cs
+++ WebUI/Controllers/PeopleController.cs
@@ -1,28 +1,18 @@
 ﻿using Application.People.Queries.GetAllPeople;
 using Domain.Entities;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PeopleController : ControllerBase
+    public class PeopleController : ApiControllerBase
     {
-        private readonly IMediator mediator;
-
-        public PeopleController(IMediator mediator)
-        {
-            this.mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<IEnumerable<Person>> GetAllPeople()
         {
             var query = new GetAllPeopleQuery();
-            return await mediator.Send(query);
+            return await Mediator.Send(query);
         }
     }
 }
