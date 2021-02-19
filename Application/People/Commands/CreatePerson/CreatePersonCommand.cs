7c21b86186dcdcc47f6e2d1871fcc2afb3d70b6f
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 17:30:35 +0300

    CreatePersonCommand was added

---
+++ Application/People/Commands/CreatePerson/CreatePersonCommand.cs
@@ -0,0 +1,47 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.People.Commands.CreatePerson
+{
+    class CreatePersonCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+    }
+
+    class CreatePersonCommandHandler : IRequestHandler<CreatePersonCommand, int>
+    {
+        private readonly IApplicationDbContext context;
+
+        public CreatePersonCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> Handle(CreatePersonCommand request, CancellationToken cancellationToken)
+        {
+            var person = new Person
+            {
+                Name = request.Name,
+                LastName = request.LastName,
+                Sex = request.Sex,
+                BirthDate = request.BirthDate
+            };
+
+            context.People.Add(person);
+            await context.SaveChanges(cancellationToken);
+
+            return person.Id;
+        }
+    }
+}
