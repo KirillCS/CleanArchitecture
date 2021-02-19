1254e7994e11eabb3f8e41ad5cdaed9a60ec7e05
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 18:05:22 +0300

    ApplicationDbContext was added

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        public async Task<int> SaveChanges(CancellationToken cancellationToken)
+        {
+            return await SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Configurations/PersonConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    class PersonConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(128);
+            builder.Property(p => p.LastName).IsRequired().HasMaxLength(128);
+            builder.Property(p => p.Sex).IsRequired();
+        }
+    }
+}
