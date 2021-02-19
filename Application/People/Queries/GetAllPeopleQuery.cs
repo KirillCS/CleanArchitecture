54272aeb184375cb8f248da93cc5be6401889167
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 17:22:08 +0300

    GetAllPeopleQuery was added

---
+++ Application/People/Queries/GetAllPeopleQuery.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.People.Queries
+{
+    class GetAllPeopleQuery : IRequest<IEnumerable<Person>>
+    { }
+
+    class GetAllPeopleQueryHandler : IRequestHandler<GetAllPeopleQuery, IEnumerable<Person>>
+    {
+        private readonly IApplicationDbContext context;
+
+        public GetAllPeopleQueryHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Person>> Handle(GetAllPeopleQuery request, CancellationToken cancellationToken)
+        {
+            var people = await context.People.ToListAsync();
+
+            return people;
+        }
+    }
+}
