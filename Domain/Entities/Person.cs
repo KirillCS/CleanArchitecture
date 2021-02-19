d241780f567962075f2f5c1eff40ad7ca8cdc621
KirillCS <KirillCS@users.noreply.github.com>
2021-02-19 17:04:14 +0300

    Person entity was added

---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+using System;
+
+namespace Domain.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+    }
+}
