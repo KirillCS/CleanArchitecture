5002762ca3f0bdd04ba05cc6f47d0bbed0118150
KirillCS <KirillCS@users.noreply.github.com>
2021-02-20 10:49:21 +0300

    Validators for commands was added

---
+++ Application/People/Commands/CreatePerson/CreatePersonCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.People.Commands.CreatePerson
+{
+    class CreatePersonCommandValidator : AbstractValidator<CreatePersonCommand>
+    {
+        public CreatePersonCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotNull().NotEmpty().WithMessage("Name cannot be null or empty")
+                .MaximumLength(128).WithMessage("Maximum length of name is 128 characters");
+
+            RuleFor(c => c.LastName)
+                .NotNull().NotEmpty().WithMessage("Last name cannot be null or empty")
+                .MaximumLength(128).WithMessage("Maximum length of last name is 128 characters");
+        }
+    }
+}
---
+++ Application/People/Commands/UpdatePerson/UpdatePersonCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.People.Commands.UpdatePerson
+{
+    class UpdatePersonCommandValidator : AbstractValidator<UpdatePersonCommand>
+    {
+        public UpdatePersonCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotNull().NotEmpty().WithMessage("Name cannot be null or empty")
+                .MaximumLength(128).WithMessage("Maximum length of name is 128 characters");
+            
+            RuleFor(c => c.LastName)
+                .NotNull().NotEmpty().WithMessage("Last name cannot be null or empty")
+                .MaximumLength(128).WithMessage("Maximum length of last name is 128 characters");
+        }
+    }
+}
