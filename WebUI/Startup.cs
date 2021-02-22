f1e2fb63087e43540a6e7f7c2a8c2ba707bdfd2a
KirillCS <KirillCS@users.noreply.github.com>
2021-02-22 23:04:03 +0300

    ClientApp was added

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -43,6 +43,10 @@ namespace WebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(b => b.AllowAnyOrigin()
+                              .AllowAnyMethod()
+                              .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
