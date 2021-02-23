3ca3a31f0984d885ae92fb7ada0db6a1a6ade411
KirillCS <KirillCS@users.noreply.github.com>
2021-02-23 12:58:23 +0300

    People components was added

--- WebUI/ClientApp/src/app/app.module.ts
+++ WebUI/ClientApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './core/components/people/people.component';
+import { DetailsComponent } from './core/components/people/details/details.component';
+import { AddingComponent } from './core/components/people/adding/adding.component';
+import { EditingComponent } from './core/components/people/editing/editing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    DetailsComponent,
+    AddingComponent,
+    EditingComponent
   ],
   imports: [
     BrowserModule,
