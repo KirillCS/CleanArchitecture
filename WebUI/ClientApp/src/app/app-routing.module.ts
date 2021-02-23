fd6e579e8b43bea694c8760d3286550fb15f12ab
KirillCS <KirillCS@users.noreply.github.com>
2021-02-23 14:07:36 +0300

    Routing was added

--- WebUI/ClientApp/src/app/app-routing.module.ts
+++ WebUI/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PeopleComponent } from './core/components/people/people.component';
+import { AddingComponent } from './core/components/people/adding/adding.component';
+import { DetailsComponent } from './core/components/people/details/details.component';
+import { EditingComponent } from './core/components/people/editing/editing.component';
+
+const routes: Routes = [
+  { path: 'people', component: PeopleComponent, },
+  { path: 'people/details', component: DetailsComponent },
+  { path: 'people/add', component: AddingComponent },
+  { path: 'people/edit', component: EditingComponent },
+  { path: '', redirectTo: '/people', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
